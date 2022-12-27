b2825d2f838d2056771d40447cb6a011c3d7b48f
Janos <mankojanos90@hotmail.com>
2022-12-27 10:39:54 +0100

    Create AddUser.js

---
+++ src/Components/Users/AddUser.js
@@ -0,0 +1,13 @@
+const AddUser = props => {
+    return (
+        <form>
+            <label htmlFor="username">
+                Username: 
+            </label>
+            <input id="username" type="text" />
+            
+        </form>
+    );
+};
+
+export default AddUser;
